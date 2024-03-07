074f1b6a411a416782381c641b2acf023e2592e4
Alexander-Santacruz <161777501+Alexander-Santacruz@users.noreply.github.com>
2024-03-07 17:41:24 -0500

    Mi primer commit

---
+++ Ankha.js
@@ -0,0 +1,90 @@
+
+let nombre="David";
+let estatura="1,72";
+
+console.log(nombre);
+console.log(estatura)
+
+
+
+let productos="Arroz"
+let precioproduct="2400";
+let unidad="3";
+let totalapagarles=precioproduct*unidad;
+   
+console.log(productos);
+console.log(precioproduct);
+console.log(unidad);
+console.log(totalapagarles);
+
+//
+
+let name="Carlos";
+let estaturas="1,75";
+
+console.log(name);
+console.log(estaturas)
+//
+
+
+let producto="Arroz";
+let precioproducto="2400";
+let unidades="3";
+let totalapagar=precioproducto*unidades;
+   
+console.log(producto);
+console.log(precioproducto);
+console.log(unidades);
+console.log(totalapagar);
+
+//
+let Nombre="Alexander";
+let Edad="20";
+console.log(`mi nombre es ${Nombre} y mi edad es ${Edad}`);
+
+  //
+
+  let estrato="3";
+  let valormatricula="";
+  
+  if(estrato >3) {
+    valormatricula=100000;
+console.log("debes pagar $"+valormatricula);
+  }
+else{
+  valormatricula=0;
+  console.log("la matricula es gratis")
+}
+//
+let tienenovia="true";
+if(tienenovia){
+  console.log("Tienes mucho gasto");}
+else{console.log(`soltero feliz`);
+    }
+                   
+let diasemana=1;
+if (diasemana=7){
+console.log("Es fin de semana");
+}
+else{
+  console.log("No es fin de semana");
+}
+
+//
+      let num1=parseInt(prompt("Ingresa el primer numero"));
+      let num2=parseInt(prompt("Ingresa el segundo numero"));
+      let multiplicacion=num1*num2;
+      alert("La multilicacion de los numeros es"
+      *multiplicacion);
+      
+      let num3=parseInt(prompt("Ingresa el primer numero"));
+      let num4=parseInt(prompt("Ingresa el segundo numero"));
+      if(num3==0){
+        alert("La division por cero no es posible");
+      }
+      else{
+        alert("La division es"+num3/num4);
+      }
+      //
+      let nombres=["Alex","Camilo","Pedro"];
+      console.log(nombres);
