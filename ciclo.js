017e25101c35fde0aa281a106926ffe2b3b91b40
Alexander-Santacruz <161777501+Alexander-Santacruz@users.noreply.github.com>
2024-03-08 09:35:06 -0500

    Trabajo con ciclo for

---
+++ ciclo.js
@@ -0,0 +1,18 @@
+for(let i=0; i <=9; i++){
+    console.log("2 x",i,"=",2*i)
+}
+//
+for(let i=1; i <=50; i++){
+    console.log(i)
+}
+//
+for(let i=0;i <=10;i++){
+    console.log(i*2);
+}
+//
+/*let numerodetabla=parseInt(prompt)(´Ingresa el numero a tabla´))
+for let i=0; i <=9 i++){
+    console.log(numerodetabla,"x",i,"=",numero de tabla*i)
+
+}*/
+
