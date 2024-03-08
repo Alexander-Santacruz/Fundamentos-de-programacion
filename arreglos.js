6af5a7c2858ad185f203f97b61747f2d489bcf94
Alexander-Santacruz <161777501+Alexander-Santacruz@users.noreply.github.com>
2024-03-08 08:18:26 -0500

    Arreglos js

---
+++ arreglos.js
@@ -0,0 +1,34 @@
+//let names=["David","Felipe","Camilo"];
+
+//console.log(nombres);//
+//Agregar elementos al arreglo de nombres
+//let nombre=["David","Felipe","Camilo"]://
+//nombres.push("Alexander");//
+//console.log(nombres(4));//
+//let Equipo=["Nacional","America","Millonarios","Cali","Junior"];
+//console.log(Equipo(5));//
+//nombres.push("America");
+
+//
+let motos=["Yamaha","Kawasaki","Honda","Suzuki","Ducati"];
+
+console.log(motos.length);
+//
+let numeros=[2,7,5,3,2];
+let suma=numeros.reduce((acumulador,valoractual)=>acumulador + valoractual,0)
+console.log(suma);
+//
+let numeromayor= Math.max(...numeros);
+console.log(numeromayor)
+//
+let frutas=["Manzana","Pera","Guayaba","Lulo","Mango"];
+let frutasinvertidas=frutas.slice().reverse();
+console.log(frutasinvertidas);
+//
+let mujeres=["Laura","Sofia","Valentina","Daniela"];
+let hombres=["Felipe","Danilo","Stywart","Brayan"];
+let personas=[...mujeres,...hombres];
+console.log(personas);
+
+let mayusculas=mujeres[2].toUpperCase();
+console.log(mayusculas);
